1756b37edcb831beaa31cd1d106c79b9a3dc8015
terminalia <mistwork@gmail.com>
2022-10-13 14:46:02 +0200

    Added Header to basic example

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup>
 import ThreeCanvas from './components/three-canvas.vue';
+import Header from './components/Header.vue';
 </script>
 
 <template>
+  <Header></Header>
+  <section>
   <ThreeCanvas></ThreeCanvas>
+</section>
 </template>
 
 <style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<script setup>
+</script>
+
+<template>
+    <header>
+        <h1>Vue 3 + ThreeJS</h1>
+    </header>
+</template>
+
+<style scoped>
+    h1 {
+        font-size: 40px;
+        color: white;
+    }
+
+    header {
+        background: #555;
+        padding: 50px;
+        width: 100%;
+        height: 100%;
+        display: grid;
+        place-content: center;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+</style>
